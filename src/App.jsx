daeafffaacc7e65dd3748b644719dbfc014387c4
Gaurav Tak <gauravtak996@gmail.com>
2023-02-16 13:03:04 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+        <h1 className=' text-blue-700 text-5xl text-center'>Hello</h1>
+        <p className=' text-center text-red-300'>You can start editing the App.jsx file</p>
+    </div>
+  )
+}
+
+export default App
